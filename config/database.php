3e51cd6ea1016729e0ff4430bd785b4c91bbca99
Feekkk <zxduke350@gmail.com>
2026-03-12 11:52:10 +0800

    update db credentials

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = ('DB_HOST') ?: 'localhost';
-$db   = ('DB_NAME') ?: 'nims';
-$user = ('DB_USER') ?: 'root';
-$pass = ('DB_PASS') ?: '';
+$host = getenv('DB_HOST') ?: 'localhost';
+$db   = getenv('DB_NAME') ?: 'nims';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
